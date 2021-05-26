e9027db95f828825c5f691458176af540318fcfc
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-05-25 23:47:40 -0300

    ci: 🎡 resume github action flows a apenas um único deploy

--- scripts/simulate-deploy.sh
+++ scripts/simulate-deploy.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-echo 'Check Format'
-npm run format:check -- --base origin/main
+echo '- run: npm ci'
+npm ci
 
-echo 'Lint Workspace & Code'
-npm run affected:lint -- --base origin/main
+echo '- name: Lint'
+npm run affected:lint --base=origin/main
 
-echo 'Unit Tests'
-npm run affected:test -- --base origin/main
+echo '- name: Test'
+npm run affected:test --base=origin/main
 
-echo 'Build affected projects'
-npm run affected:build  -- --base origin/main --with-deps --prod
+echo '- name: Build'
+npm run affected:build --base=origin/main --prod
 
-echo 'Release affected libraries'
-npm run affected -- --target release --debug --dry-run --base origin/main
+echo '- name: Release'
+npx nx affected --target release --base=origin/main
+
+# echo 'Check Format'
+# npm run format:check -- --base origin/main
+
+# echo 'Lint Workspace & Code'
+# npm run affected:lint -- --base origin/main
+
+# echo 'Unit Tests'
+# npm run affected:test -- --base origin/main
+
+# echo 'Build affected projects'
+# npm run affected:build  -- --base origin/main --with-deps --prod
+
+# echo 'Release affected libraries'
+# npm run affected -- --target release --debug --dry-run --base origin/main
+
