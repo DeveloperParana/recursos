557188b587d0f3b9a0a1043483efcefd430dd151
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-06-10 00:40:22 -0300

    💡 refactor(devector): http service types

--- apps/devector/src/app/data-access/http-data.service.ts
+++ apps/devector/src/app/data-access/http-data.service.ts
@@ -3,31 +3,31 @@ import { map } from 'rxjs/operators'
 import { ajax } from 'rxjs/ajax'
 
 export class HttpDataService {
-  get<T = any>(input: HttpConfig) {
+  get<T = unknown>(input: HttpConfig) {
     return ajax(this._getConfig(input, 'GET')).pipe(
       map((response) => response.response as T)
     )
   }
 
-  post<T = any>(input: HttpConfig) {
+  post<T = unknown>(input: HttpConfig) {
     return ajax(this._getConfig(input, 'POST')).pipe(
       map((response) => response.response as T)
     )
   }
 
-  pur<T = any>(input: HttpConfig) {
+  pur<T = unknown>(input: HttpConfig) {
     return ajax(this._getConfig(input, 'PUT')).pipe(
       map((response) => response.response as T)
     )
   }
 
-  patch<T = any>(input: HttpConfig) {
+  patch<T = unknown>(input: HttpConfig) {
     return ajax(this._getConfig(input, 'PATCH')).pipe(
       map((response) => response.response as T)
     )
   }
 
-  delete<T = any>(input: HttpConfig) {
+  delete<T = unknown>(input: HttpConfig) {
     return ajax(this._getConfig(input, 'DELETE')).pipe(
       map((response) => response.response as T)
     )
