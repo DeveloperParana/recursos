f314e96fe28aa16a5e9b2bf259e7ad5b716d9af6
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-06-10 00:40:22 -0300

    💡 refactor(devector): http service types

--- packages/web/forms/src/lib/web-forms.d.ts
+++ packages/web/forms/src/lib/web-forms.d.ts
@@ -1,10 +1,6 @@
 import { FormFieldBase } from './form-field-base'
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'form-field': FormFieldBase
-  }
-
   type EventWithTarget<E = Event, T = HTMLElement> = E & {
     target: T
     currentTarget: T
